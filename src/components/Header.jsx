36a93dd72fe72a19a5a296d4b0d3b674983dc0ba
Aliaksei Kushnir <1310311a@gmail.com>
2020-11-17 21:51:00 +0300

    Первый коммит

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+const Header=()=>{
+    return (
+<header className='header'>
+        <img src='https://cdn.worldvectorlogo.com/logos/vk-1.svg'/>
+      </header>
+      )
+}
+
+
+export default Header
---
+++ src/components/content.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Content =()=>{
+
+return(
+
+<div className='content'>
+        <div>
+            фон
+        </div>
+        <div>
+            ava +decription
+        </div>
+        <div>
+            My posts
+            <div>
+                newPost
+            </div>
+            <div>post1</div>
+            <div>post2</div>
+        </div>
+      </div>
+)
+
+}
+
+export default Content
